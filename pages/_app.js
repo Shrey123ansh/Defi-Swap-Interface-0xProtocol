e36c4f65e8b92dd0f6810f9f3023f1cc9c90c139
Shrey123ansh <shreyann56sh@gmail.com>
2024-08-25 03:55:34 +0530

    first-commit

---
+++ pages/_app.js
@@ -0,0 +1,19 @@
+import { NextUIProvider } from "@nextui-org/react";
+import { createTheme } from "@nextui-org/react";
+import "../styles/globals.css";
+import Defiswap from "./defiswap";
+
+const darkTheme = createTheme({
+  type: "dark",
+});
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <NextUIProvider theme={darkTheme}>
+      <Component {...pageProps} />
+      <Defiswap />
+    </NextUIProvider>
+  );
+}
+
+export default MyApp;
